5df57ada7a54ef944c6b55ee8afbef4bb0e8471a
Agil Atakishiyev <aqilatakishiyev1@gmail.com>
2022-06-27 01:03:39 +0400

    added files

---
+++ assets/js/main.js
@@ -0,0 +1,146 @@
+// Write your code here
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(i);
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+// for (const index in numbers) {
+//   console.log(index);
+// }
+
+// Data Types
+// Reference Type - object (Array, Object)
+// Value Type - number, BigInt, string, Symbol, undefined, boolean
+
+// Data Types
+// Non-primitive - object
+// Primitive - number, BigInt, string, Symbol, undefined, boolean
+
+// let i = 0;
+
+// while (i < 100) {
+//   console.log(i);
+//   i++;
+// }
+
+// let j = 0;
+
+// do {
+//   console.log(j);
+//   j++;
+// } while (j > 100);
+
+// let num = 5;
+// try {
+//   num = 10;
+// } catch (err) {
+//   console.log(err);
+// } finally {
+//   console.log("finished");
+// }
+
+// console.log(num);
+
+// const userWeekdayChoice = prompt("Please enter a week day");
+
+// switch (userWeekdayChoice) {
+//   case "Monday":
+//     console.log("it is monday again");
+//     break;
+//   case "Tuesday":
+//     console.log("it is tuesday");
+//     break;
+//   case "Wednesday":
+//     console.log("it is wednesday");
+//     break;
+//   case "Thursday":
+//     console.log("it is thursday");
+//     break;
+//   default:
+//     console.log("wrong input");
+//     break;
+// }
+
+// const num = 1;
+
+// switch (num) {
+//   case 1:
+//   case "bir":
+//     console.log("today is Monday");
+//     break;
+//   case 2:
+//     console.log("today is Tuesday");
+//     break;
+//   default:
+//     console.log("wrong input");
+// }
+
+// const examScore = prompt("Dear user, please enter your exam score");
+
+// if (examScore < 0 || examScore > 100) {
+//   alert("Bele bal olmur qaqaw");
+// } else if (examScore >= 91 && examScore <= 100) {
+//   alert("You got an A");
+// } else if (examScore >= 81 && examScore < 91) {
+//   alert("You got a B");
+// } else if (examScore >= 71 && examScore < 81) {
+//   alert("You got a C");
+// } else if (examScore >= 61 && examScore < 71) {
+//   alert("You got a D");
+// } else if (examScore >= 51 && examScore < 61) {
+//   alert("You got a D");
+// } else {
+//   alert("Welcome to 25%");
+// }
+
+// if ("") {
+//   console.log("Hello p412");
+// }
+
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof -100);
+// console.log(typeof 0);
+// console.log(typeof 100);
+// console.log(typeof false);
+// console.log(typeof [false, false]);
+// console.log(typeof [false, false]);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+/* 
+    Truthy values -
+        - Negative & Positive integers (-1, -100, 100, 300)
+        - {}, { firstname: "Rauf" }, { age: 20 }
+        - [], [1,2,3], ["salam", "saol"], [false, false];
+        - " ", "    ", "hello world", ' '
+        - true
+    Falsy values  
+        - undefined
+        - null
+        - false
+        - 0
+        - "", '', 
+*/
+
+// Ternary operator
+
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean(" "));
+// console.log(Boolean("salam"));
+// console.log(Boolean(5));
+// console.log(Boolean(-5));
+
+// console.log("-------------");
+
+// console.log(Boolean(0));
+// console.log(Boolean(false));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
